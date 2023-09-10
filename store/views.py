e343c0d962597fa72990ad66842e5e8d5e512591
3La221 <abacha3la2@gmail.com>
2023-09-10 08:45:55 +0100

    before-templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+	context = {}
+	return render(request,'store/main.html',context)
+
+def store(request):
+	context = {}
+	return render(request,'store/store.html',context)
+
+
+def checkout(request):
+	context = {}
+	return render(request,'store/checkout.html',context)
+
+def product(request):
+	context = {}
+	return render(request,'store/product.html',context)
